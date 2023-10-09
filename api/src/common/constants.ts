c625c70021cbbeb01429c1bc018fb2036b4fd77c
Rohan Jadvani <5459049+rohanjadvani@users.noreply.github.com>
2023-10-09 16:00:30 -0400

    Fix address (#25)

--- api/src/common/constants.ts
+++ api/src/common/constants.ts
@@ -5,4 +5,4 @@ export const SEPOLIA_EXPLORER_URL = 'https://sepolia.etherscan.io';
 export const SEPOLIA_BLOCK_TIME_MS = 15 * 1000;
 
 export const WIRON_CONTRACT_ADDRESS =
-  '0x5c6c596cD1866315117d783af9045d9eB8F44802';
+  '0x3de166740d64d522abfda77d9d878dfedfdeeede';
