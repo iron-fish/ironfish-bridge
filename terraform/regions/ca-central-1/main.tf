52ad2dcc1d5220e1eeca07cf2f4a0734f31f0a4f
jowparks <joe@iflabs.network>
2023-11-02 12:10:13 -0700

    chore: cleanup terraform deployment, add variables for configuration of services in the deployment region (#66)

--- terraform/regions/ca-central-1/main.tf
+++ terraform/regions/ca-central-1/main.tf
@@ -9,50 +9,6 @@ terraform {
   }
 }
 
-variable "az" {
-  default = "ca-central-1"
-}
-
-variable "bridge_name" {
-  default = "ironfish-bridge"
-}
-
-
-// the value below should correspond to instance connect for the region(s) you are deploying to
-// https://docs.aws.amazon.com/vpc/latest/userguide/aws-ip-ranges.html
-variable "ec2_instance_connect_cidr" {
-  default = "35.183.92.176/29"
-}
-
-variable "api_token" {
-  default = "test"
-}
-
-variable "bridge_address" {
-  default = "4446bf78457d7b256f16b645fa0a3d4212c32b493de90ad920521f89808d022a"
-}
-
-variable "incoming_view_key" {
-  default = "a72c04ddb2cbe87f1f4fe1eee6b7cac64a43316285de48f793c238bbb0793707"
-}
-
-variable "outgoing_view_key" {
-  default = "42fe52470af73cde0c82236381d4d21aa9eaba6de14352f8aeb194b730a0d68c"
-  
-}
-
-variable "rpc_auth_token" {
-  default = "d3893f3acb07511796fae3c198e84883db8b3c8fdd819096cdfa90dcbe36055e"
-}
-
-variable "api_name" {
-  default = "ironfish-bridge-api"
-}
-
-variable "node_name" {
-  default = "ironfish-bridge-node"
-}
-
 
 
 provider "aws" {
@@ -75,8 +31,8 @@ module "node" {
   aws_vpc                           = module.aws.vpc_ironfish
   environment_name                  = var.node_name
   node_name                         = var.node_name
-  instance_type                     = "t3.small"
-  rpc_allowed_cidr_blocks           = ["0.0.0.0/0"]
+  instance_type                     = var.node_instance_type
+  rpc_allowed_cidr_blocks           = var.rpc_allowed_cidr_blocks
   rpc_auth_token                    = var.rpc_auth_token
   network_id                        = 1
   instance_connect_cidrs            = [var.ec2_instance_connect_cidr]
@@ -89,8 +45,8 @@ module "relay" {
   aws_route53_zone                  = module.aws.route53_zone_ironfish
   aws_subnet_private                = module.aws.subnet_ironfish_private
   aws_vpc                           = module.aws.vpc_ironfish
-  environment_name                  = "${var.bridge_name}-relay"
-  instance_type                     = "t3.small"
+  environment_name                  = var.relay_name
+  instance_type                     = var.relay_instance_type
   instance_connect_cidrs            = [var.ec2_instance_connect_cidr]
   // hardcoding endpoint instead of ${module.api.endpoint_url} because of circular dependency
   // forcing install of linux nodejs binary, not installing automatically
@@ -102,8 +58,8 @@ module "release" {
   aws_route53_zone                  = module.aws.route53_zone_ironfish
   aws_subnet_private                = module.aws.subnet_ironfish_private
   aws_vpc                           = module.aws.vpc_ironfish
-  environment_name                  = "${var.bridge_name}-release"
-  instance_type                     = "t3.small"
+  environment_name                  = var.release_name
+  instance_type                     = var.release_instance_type
   instance_connect_cidrs            = [var.ec2_instance_connect_cidr]
   // hardcoding endpoint instead of ${module.api.endpoint_url} because of circular dependency
   // forcing install of linux nodejs binary, not installing automatically
@@ -116,29 +72,29 @@ module "api" {
 
   # Application and region
   environment_name                  = var.api_name
-  aws_region                        = "ca-central-1"
+  aws_region                        = var.az
   aws_route53_zone                  = module.aws.route53_zone_ironfish
   aws_subnet_private                = module.aws.subnet_ironfish_private
   aws_vpc                           = module.aws.vpc_ironfish
   ingress_security_groups           = [module.relay.security_group, module.release.security_group]
-  instance_type                     = "t3.small"
+  instance_type                     = var.api_instance_type
 
   # API ENV Variables
-  DB_NAME                                    = "ironfish_bridge"
-  DB_USER                                    = "ironfish"
-  DB_PASSWORD                                = "ironfish"
-  GRAPHILE_CONCURRENCY                       = 10
-  IRONFISH_BRIDGE_ADDRESS                    = var.bridge_address
-  IRONFISH_BRIDGE_API_KEY                    = var.api_token
-  NODE_ENV                                   = "development"
-  REFRESH_WIRON_TRANSFERS_PERIOD_MINUTES     = 2
-  REFRESH_TEST_USDC_TRANSFERS_PERIOD_MINUTES = 2
-  TEST_USDC_DEPLOYER_PRIVATE_KEY             = "test"
-  TEST_USDC_DEPOSIT_ADDRESS                  = "test"
-  TEST_USDC_FINALITY_HEIGHT_RANGE            = 10
-  TEST_USDC_QUERY_HEIGHT_RANGE               = 100
-  WIRON_DEPLOYER_PRIVATE_KEY                 = "test"
-  WIRON_DEPOSIT_ADDRESS                      = "test"
-  WIRON_FINALITY_HEIGHT_RANGE                = 10
-  WIRON_QUERY_HEIGHT_RANGE                   = 100
+  DB_NAME                                    = var.DB_NAME
+  DB_USER                                    = var.DB_USER
+  DB_PASSWORD                                = var.DB_PASSWORD
+  GRAPHILE_CONCURRENCY                       = var.GRAPHILE_CONCURRENCY
+  IRONFISH_BRIDGE_ADDRESS                    = var.IRONFISH_BRIDGE_ADDRESS
+  IRONFISH_BRIDGE_API_KEY                    = var.IRONFISH_BRIDGE_API_KEY
+  NODE_ENV                                   = var.NODE_ENV
+  REFRESH_WIRON_TRANSFERS_PERIOD_MINUTES     = var.REFRESH_WIRON_TRANSFERS_PERIOD_MINUTES
+  REFRESH_TEST_USDC_TRANSFERS_PERIOD_MINUTES = var.REFRESH_TEST_USDC_TRANSFERS_PERIOD_MINUTES
+  TEST_USDC_DEPLOYER_PRIVATE_KEY             = var.TEST_USDC_DEPLOYER_PRIVATE_KEY
+  TEST_USDC_DEPOSIT_ADDRESS                  = var.TEST_USDC_DEPOSIT_ADDRESS
+  TEST_USDC_FINALITY_HEIGHT_RANGE            = var.TEST_USDC_FINALITY_HEIGHT_RANGE
+  TEST_USDC_QUERY_HEIGHT_RANGE               = var.TEST_USDC_QUERY_HEIGHT_RANGE
+  WIRON_DEPLOYER_PRIVATE_KEY                 = var.WIRON_DEPLOYER_PRIVATE_KEY
+  WIRON_DEPOSIT_ADDRESS                      = var.WIRON_DEPOSIT_ADDRESS
+  WIRON_FINALITY_HEIGHT_RANGE                = var.WIRON_FINALITY_HEIGHT_RANGE
+  WIRON_QUERY_HEIGHT_RANGE                   = var.WIRON_QUERY_HEIGHT_RANGE
 }
---
+++ terraform/regions/ca-central-1/variables.tf
@@ -0,0 +1,173 @@
+
+variable "az" {
+  default = "ca-central-1"
+}
+// the value below should correspond to instance connect for the region(s) you are deploying to
+// https://docs.aws.amazon.com/vpc/latest/userguide/aws-ip-ranges.html
+variable "ec2_instance_connect_cidr" {
+  default = "35.183.92.176/29"
+}
+
+variable "api_instance_type" {
+  default = "t3.small"
+}
+
+variable "relay_instance_type" {
+  default = "t3.small"
+}
+
+variable "release_instance_type" {
+  default = "t3.small"
+}
+
+variable "node_instance_type" {
+  default = "t3.small"
+}
+
+variable "api_token" {
+  default = "test"
+}
+
+variable "bridge_address" {
+  default = "4446bf78457d7b256f16b645fa0a3d4212c32b493de90ad920521f89808d022a"
+}
+
+variable "incoming_view_key" {
+  default = "a72c04ddb2cbe87f1f4fe1eee6b7cac64a43316285de48f793c238bbb0793707"
+}
+
+variable "outgoing_view_key" {
+  default = "42fe52470af73cde0c82236381d4d21aa9eaba6de14352f8aeb194b730a0d68c"
+  
+}
+
+variable "rpc_auth_token" {
+  default = "d3893f3acb07511796fae3c198e84883db8b3c8fdd819096cdfa90dcbe36055e"
+}
+
+variable "rpc_allowed_cidr_blocks" {
+  default = ["0.0.0.0/0"]
+}
+
+// naming of elastic beanstalk applications
+
+variable "api_name" {
+  default = "ironfish-bridge-api"
+}
+
+variable "node_name" {
+  default = "ironfish-bridge-node"
+}
+
+variable "release_name" {
+  default = "ironfish-bridge-release"
+}
+
+variable "relay_name" {
+  default = "ironfish-bridge-relay"
+}
+
+
+
+
+# API ENV variables
+
+// for environment variables in the api application
+variable "DB_NAME" {
+    default = "ironfish_bridge"
+    type = string
+}
+
+variable "DB_USER" {
+    default = "ironfish"
+    type = string
+}
+
+variable "DB_PASSWORD" {
+    default = "ironfish"
+    type = string
+}
+
+variable "GRAPHILE_CONCURRENCY" {
+  description = "Concurrency level for Graphile"
+  type        = number
+  default     = 10
+}
+
+variable "IRONFISH_BRIDGE_ADDRESS" {
+  description = "IronFish bridge address"
+  type        = string
+  default     = "test"
+}
+
+variable "IRONFISH_BRIDGE_API_KEY" {
+  description = "API key for the IronFish bridge"
+  type        = string
+  default     = "test"
+}
+
+variable "NODE_ENV" {
+  description = "Node environment"
+  type        = string
+  default     = "development"
+}
+
+variable "REFRESH_WIRON_TRANSFERS_PERIOD_MINUTES" {
+  description = "Refresh period for WIRON transfers in minutes"
+  type        = number
+  default     = 2
+}
+
+variable "REFRESH_TEST_USDC_TRANSFERS_PERIOD_MINUTES" {
+  description = "Refresh period for test USDC transfers in minutes"
+  type        = number
+  default     = 2
+}
+
+variable "TEST_USDC_DEPLOYER_PRIVATE_KEY" {
+  description = "Private key for test USDC deployer"
+  type        = string
+  default     = "test"
+}
+
+variable "TEST_USDC_DEPOSIT_ADDRESS" {
+  description = "Deposit address for test USDC"
+  type        = string
+  default     = "test"
+}
+
+variable "TEST_USDC_FINALITY_HEIGHT_RANGE" {
+  description = "Finality height range for test USDC"
+  type        = number
+  default     = 10
+}
+
+variable "TEST_USDC_QUERY_HEIGHT_RANGE" {
+  description = "Query height range for test USDC"
+  type        = number
+  default     = 100
+}
+
+variable "WIRON_DEPLOYER_PRIVATE_KEY" {
+  description = "Private key for WIRON deployer"
+  type        = string
+  default     = "test"
+}
+
+variable "WIRON_DEPOSIT_ADDRESS" {
+  description = "Deposit address for WIRON"
+  type        = string
+  default     = "test"
+}
+
+variable "WIRON_FINALITY_HEIGHT_RANGE" {
+  description = "Finality height range for WIRON"
+  type        = number
+  default     = 10
+}
+
+variable "WIRON_QUERY_HEIGHT_RANGE" {
+  description = "Query height range for WIRON"
+  type        = number
+  default     = 100
+}
